7bd7ca48a7e9eabfce0743f03fb0e2711bd2906b
salvadodo <salvadfloz@gmail.com>
2021-12-13 20:31:10 -0600

    Corrección charset y sp

--- backPaginaMuestra.php
+++ backPaginaMuestra.php
@@ -1,11 +1,14 @@
 <?php
     require "cadConPaginaMuestra.php";
-    $query="sp_selectalldueñosInnercasas";
+    mysqli_set_charset($con, "utf8");
+    $query="CALL sp_selectalldueñosInnercasas()";
+    // $query="select * from dueños inner join casas on idDueño=Dueño";
+    // $query="select * from casas";
     $datosBD=mysqli_query($con, $query);
-    while($fila=mysqli_fetch_array($datosBD, MYSQL_BOTH)){
-        echo "<div>".$fila["tipodeCasa"]."</div>" . "<div>".$fila["Ubicacion"]."</div>" . "<div>".$fila["Descripcion"]."</div>" .
-        "<div>".$fila["Dueño"]."</div>" . "<div>".$fila["Nombre"]."</div>" .
-        "<div>".$fila["Edad"]."</div>" . "<div>".$fila["Direccion"]."</div>";
+    while($fila=mysqli_fetch_array($datosBD, MYSQLI_BOTH)){
+            echo "<div>".$fila["tipodeCasa"]."</div>" . "<div>".$fila["Ubicacion"]."</div>".
+            "<div>".$fila["Descripcion"]."</div>" . "<div>".$fila["Dueño"]."</div>".
+            "<div>".$fila["Nombre"]."</div>" . "<div>".$fila["Edad"]."</div>" . "<div>".$fila["Direccion"]."</div>";    
     }
     mysqli_close($con);
 ?>
